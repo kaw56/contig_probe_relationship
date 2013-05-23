79df7b72d0acbf3b3e6089a5320663884452e9e1
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-23 14:02:13 +0100

    Code for loading up data

--- 1_load.R
+++ 1_load.R
@@ -1,4 +1,10 @@
-# load up data needed for analysis and necessary packages
-
 ## @knitr load
 library(knitr)
+
+# read in the chosen probe-contig pairs
+probe_contig_key <- read.table("Data/array_probeset_key.txt", header = TRUE)
+colnames(probe_contig_key) <- c("Probe", "Contig")
+
+# read in the blast results
+contig_blast <- read.table("Data/454_contigs_for_arrays.fastaRESULTS_summary.txt")
+colnames(contig_blast) <- c("Contig", "Probe", "evalue")

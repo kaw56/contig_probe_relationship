7d359c2819b74405cc8b0a16e0303bbc1eeb77cc
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-24 08:58:49 +0100

    Wrote Analysis code
    playing around with some measures of the distribution
    of evalues to see what sort of cut off Brian used for
    selecting his representative probes

--- 4_do.R
+++ 4_do.R
@@ -1,1 +1,16 @@
-# perform analysis here generate graphs and tables and so on
+# mean evalue
+
+mean_evalue <- mean(probes_in_brians$evalue)
+
+range_evalue <- range(probes_in_brians$evalue)
+
+lowest  <- range_evalue[1]
+highest <- range_evalue[2]
+
+subset(probes_in_brians, evalue == highest)
+
+stupid <- rep(stupid, length(probes_in_brians$Probe))
+
+probes_in_brians <- cbind(probes_in_brians, stupid)
+
+ggplot(probes_in_brians, aes(x= stupid, y = evalue)) + geom_boxplot()

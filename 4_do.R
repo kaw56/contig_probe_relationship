8ce0bc64b8ce6bbabcf880b6aaa116a73a3bf25c
Katherine <kwhite@mrc-lmb.cam.ac.uk>
2013-05-29 13:47:05 +0100

    Details about Brian's probes

--- 4_do.R
+++ 4_do.R
@@ -1,3 +1,27 @@
+# total number of probes that brian used
+total_probes <- length(probe_contig_key$Probe)
+# number of forward and reverse probes
+total_each_type <- ddply(probe_contig_data, "type",
+                         summarise,
+                         n = length(Contig))
+
+# average number of probes per contig 
+total_each_contig <- ddply(probe_contig_data, "Contig",
+                           summarise,
+                           Probe_num = length(Probe))
+
+subset(total_each_contig, n == 1)
+subset(total_each_contig, n == 3)
+
+# the number of contigs per probe
+total_each_probe <- ddply(probe_contig_data, "Probe",
+                          summarise,
+                          Contig_num = length(Contig))
+
+mean(total_each_probe$Contig_num)
+
+# see if they use the same probes (somehow)
+
 # mean evalue
 
 mean_evalue <- mean(probes_in_brians$evalue)
